a824ffd696ca94ea1329c71580f2e63be908f3b5
Mariusz Brzeski <marianob85work@gmail.com>
2017-06-05 21:53:22 +0200

    add HuaweiHiLinkAPI

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -57,6 +57,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/httpjson"
 	_ "github.com/influxdata/telegraf/plugins/inputs/icinga2"
 	_ "github.com/influxdata/telegraf/plugins/inputs/infiniband"
+	_ "github.com/influxdata/telegraf/plugins/inputs/huawei_hilink_api"
 	_ "github.com/influxdata/telegraf/plugins/inputs/influxdb"
 	_ "github.com/influxdata/telegraf/plugins/inputs/influxdb_listener"
 	_ "github.com/influxdata/telegraf/plugins/inputs/internal"
